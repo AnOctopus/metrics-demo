dd91ec5c5c6799543372a4d39641cfb0e7080402
Amanda Walker <amanda@amandawalker.io>
2021-11-05 14:34:24 -0700

    Copy in memory-using code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import timeit
 
 ss = st.session_state
-ss.foo = 5
 
-st.write(ss)
+start = timeit.default_timer()
+rows = st.number_input("how many rows", 0)
+
+
+def make_df(rows):
+    dates = pd.date_range("20130101", periods=rows)
+    df = pd.DataFrame(np.random.randn(rows, 4), index=dates, columns=list("ABCD"))
+    return df
+
+
+df = make_df(rows)
+ss[rows] = df
+st.write(df)
+end = timeit.default_timer()
+st.write(end - start)
